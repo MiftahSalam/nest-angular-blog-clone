bc63e346d8b763f0801f8e999bfea6b975c90f15
Miftah Salam <salam.miftah@gmail.com>
2022-01-20 15:24:29 +0700

    setup typeorm module and configurtion

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+
+import ormconfig = require('./config/ormconfig');
+
+// console.log(ormconfig);
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormconfig[0]), AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
