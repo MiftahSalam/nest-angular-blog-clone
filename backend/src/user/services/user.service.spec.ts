6a54a2c618da530b7c544f427ce2d1cb8269d27d
Miftah Salam <salam.miftah@gmail.com>
2022-01-21 15:57:11 +0700

    findUserByName implementation and testing

--- backend/src/user/services/user.service.spec.ts
+++ backend/src/user/services/user.service.spec.ts
@@ -1,44 +1,94 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import {
-  getConnectionToken,
-  getRepositoryToken,
-  TypeOrmModule,
-} from '@nestjs/typeorm';
+import { getConnectionToken, TypeOrmModule } from '@nestjs/typeorm';
 import { Connection, Repository } from 'typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { UserService } from './user.service';
+import { Output } from '../../core/output';
+import { UserDto } from '../dtos/user.dto';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
 import ormconfig = require('../../config/ormconfig');
 
 describe('UserService', () => {
   let service: UserService;
   let connection: Connection;
+  let testUser: CreateUserDto = {
+    username: 'test-user',
+    email: 'test@yahoo.com',
+    password: '123456',
+    fullname: 'Test User',
+    image_url: 'http://www.test.user',
+  };
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         TypeOrmModule.forRoot(ormconfig[0]),
         TypeOrmModule.forFeature([UserEntity]),
       ],
-      providers: [
-        UserService,
-        Repository,
-        {
-          provide: getRepositoryToken(UserEntity),
-          useValue: {},
-        },
-      ],
+      providers: [UserService, Repository],
     }).compile();
 
     service = module.get<UserService>(UserService);
     connection = await module.get(getConnectionToken());
+    await connection
+      .createQueryBuilder(UserEntity, 'users')
+      .insert()
+      .into(UserEntity)
+      .values([
+        {
+          username: testUser.username,
+          email: testUser.email,
+          password: testUser.password,
+          fullname: testUser.fullname,
+          image_url: testUser.image_url,
+        },
+      ])
+      .execute();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  afterEach(async () => {
+  // it('should be get one user async', async () => {
+  //   const user = await service.findUserByNameAsync('miftah-salam');
+  // });
+
+  it('should be get one user', (done: jest.DoneCallback) => {
+    service.findUserByName(testUser.username).subscribe((user: UserDto) => {
+      // console.log('Test-User-Service-findUserByName user', user);
+
+      expect(user.username).toEqual(testUser.username);
+      expect(user.email).toEqual(testUser.email);
+      expect(user.fullname).toEqual(testUser.fullname);
+      expect(user.image_url).toEqual(testUser.image_url);
+
+      done();
+    });
+  });
+
+  // it('should be create one user', (done: jest.DoneCallback) => {
+  //   const user = {
+  //     username: 'miftah-salam',
+  //     password: '123456',
+  //     email: 'salam.miftah@gmail.com',
+  //     fullname: 'Miftah Salam',
+  //     image_url: 'https://www.test.com',
+  //   };
+  //   service.createUser(user).subscribe((out: Output) => {
+  //     console.log('Test-User-Service-createUser out', out);
+  //     done();
+  //   });
+  // });
+
+  afterAll(async () => {
+    await connection
+      .createQueryBuilder(UserEntity, 'users')
+      .delete()
+      .from(UserEntity)
+      .where('username = :username', { username: testUser.username })
+      .execute();
     await connection.close();
   });
 });
--- backend/src/user/services/user.service.ts
+++ backend/src/user/services/user.service.ts
@@ -1,14 +1,65 @@
-import { Injectable } from '@nestjs/common';
-import { from, Observable } from 'rxjs';
+import { Injectable, Logger } from '@nestjs/common';
+import { from, Observable, switchMap, of, tap } from 'rxjs';
+import { Output } from '../../core/output';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dtos/create-user.dto';
 import { UserDto } from '../dtos/user.dto';
 import { UserEntity } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userService: Repository<UserEntity>) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userService: Repository<UserEntity>,
+  ) {}
 
+  createUser(user: CreateUserDto): Observable<Output> {
+    console.log('User-Service-createUser');
+
+    return from(this.findUserByName(user.username)).pipe(
+      switchMap((user: UserDto) => {
+        console.log('User-Service-createUser user', user);
+
+        const out: Output = new Output();
+        out.status = 'Created';
+        out.message = 'User create';
+        out.data = {};
+
+        return of(out);
+      }),
+    );
+    // const out: Output = new Output;
+    // out.status = "Created";
+    // out.message = "User created";
+    // out.data = {};
+
+    // return out;
+  }
   findUserById(id: string): Observable<UserDto> {
     return from(this.userService.findOne(id));
+  }
+
+  findUserByName(username: string): Observable<UserDto> {
+    // console.log('User-Service-findUserByName username', username);
+    return from(
+      this.userService.findOne({ where: { username: username } }),
+    ).pipe(
+      switchMap((user: UserEntity) => {
+        return from(user.toDto());
+      }),
+    );
+  }
+
+  async findUserByNameAsync(username: string): Promise<UserDto> {
+    console.log('User-Service-findUserByName username', username);
+
+    const user = await this.userService.findOne({
+      where: { username: username },
+    });
+
+    console.log('User-Service-findUserByName user', user);
+
+    return user;
   }
 }
