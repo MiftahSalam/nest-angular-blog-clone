b8439cd50324f52d446ac5c0425f51bbcc0cec0a
Miftah Salam <salam.miftah@gmail.com>
2022-01-22 09:31:14 +0700

    first implementation

--- backend/src/user/output/create-user.output.ts
+++ backend/src/user/output/create-user.output.ts
@@ -1,3 +1,9 @@
-import { Output } from 'src/core/output';
+import { OutputDto } from 'src/core/output';
 
-export interface CreateUserOutput extends Output {}
+export interface CreateUserOutput extends OutputDto {
+  id: string;
+  username: string;
+  fullname: string;
+  image_url: string;
+  email: string;
+}
