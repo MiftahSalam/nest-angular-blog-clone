911ef80eb2f7a59866b788d12866aaccb4015029
Miftah Salam <salam.miftah@gmail.com>
2022-01-22 14:49:46 +0700

    implement all app exceptions

---
+++ backend/src/core/allexception.filter.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { PresenterOutput } from './presenter';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: any, host: ArgumentsHost) {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const presenter: PresenterOutput = {
+      status: httpStatus,
+      message:
+        exception instanceof HttpException ? exception.message : 'Server error',
+      data: {},
+    };
+    httpAdapter.reply(ctx.getResponse(), presenter, httpStatus);
+  }
+}
