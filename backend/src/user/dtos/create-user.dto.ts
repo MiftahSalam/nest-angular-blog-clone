67fdffaed016bc1680da8bb2153a500e0b7b0348
Miftah Salam <salam.miftah@gmail.com>
2022-01-22 09:28:46 +0700

    add class-validator

--- backend/src/user/dtos/create-user.dto.ts
+++ backend/src/user/dtos/create-user.dto.ts
@@ -1,7 +1,19 @@
-export interface CreateUserDto {
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
   username: string;
-  password: string;
-  email: string;
+
+  @IsNotEmpty()
   fullname: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
   image_url: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 }
