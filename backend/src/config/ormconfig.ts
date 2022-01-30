4d8cd6995c03465a85a0635c8c4cbfbece9f780d
Miftah Salam <salam.miftah@gmail.com>
2022-01-31 04:26:10 +0700

    setup ormconfig base on NODE_ENV

--- backend/src/config/ormconfig.ts
+++ backend/src/config/ormconfig.ts
@@ -5,11 +5,26 @@ export = [
   {
     name: 'default',
     type: 'postgres',
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT) || 5432,
-    username: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'postgres',
-    database: process.env.DATABASE_NAME || 'ng_nest_blog',
+    host:
+      process.env.NODE_ENV === 'production'
+        ? process.env.DATABASE_HOST
+        : 'localhost',
+    port:
+      process.env.NODE_ENV === 'production'
+        ? parseInt(process.env.DATABASE_PORT)
+        : 5432,
+    username:
+      process.env.NODE_ENV === 'production'
+        ? process.env.DATABASE_USERNAME
+        : 'postgres',
+    password:
+      process.env.NODE_ENV === 'production'
+        ? process.env.DATABASE_PASSWORD
+        : 'postgres',
+    database:
+      process.env.NODE_ENV === 'production'
+        ? process.env.DATABASE_NAME
+        : 'ng_nest_blog',
     entities: [
       join(__dirname, '..', '/**/*.entity{.ts,.js}'),
       'dist/**/*.entity{.ts,.js}',
