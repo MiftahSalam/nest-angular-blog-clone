c25ba47b4375fbd3682e1e65116a56537b5af75f
Miftah Salam <salam.miftah@gmail.com>
2022-02-02 08:29:25 +0700

    delete app controller, service and setup static

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,26 +2,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_FILTER } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { AllExceptionsFilter } from './core/allexception.filter';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BlogModule } from './blog/blog.module';
 import ormconfig = require('./config/ormconfig');
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'frontend') }),
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(ormconfig[0]),
     AuthModule,
     UserModule,
     BlogModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_FILTER,
       useClass: AllExceptionsFilter,
