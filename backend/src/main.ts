18139debd179ce6ddbf181b460e01e473afbb122
Miftah Salam <salam.miftah@gmail.com>
2022-02-02 07:30:02 +0700

    install and implement helmet, express rate limit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
+  app.use(helmet());
   app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    }),
+  );
   await app.listen(port, () => {
     console.log('Application running on port', port);
   });
