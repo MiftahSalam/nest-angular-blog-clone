baf3d2ce2b75157c91c507cdf6f4bf3540e4829f
Miftah Salam <salam.miftah@gmail.com>
2022-02-02 17:50:19 +0700

    add header, home, allblogs, customerror component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AllBlogsComponent } from './component/all-blogs/all-blogs.component';
+import { CustomErrorHandle } from './core/ErrorHandle';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AllBlogsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    HttpClient,
+    { provide: ErrorHandler, useClass: CustomErrorHandle },
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
