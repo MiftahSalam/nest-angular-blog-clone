2e7f3e55518d36cde0ce7f94bcc02a3224aaecb9
Miftah Salam <salam.miftah@gmail.com>
2022-02-02 19:28:44 +0700

    build to backend (debugging cors)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,33 @@ import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
 
 import { AppModule } from './app.module';
+import { ForbiddenException } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  const WHITE_LIST = JSON.parse(<string>process.env.CORS_WHITELIST) as {
+    whitelist: [];
+  };
+  console.log(WHITE_LIST);
 
   app.use(helmet());
-  app.enableCors();
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (WHITE_LIST.whitelist.indexOf[origin] !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new ForbiddenException('Not allowed by cors'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
+
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000,
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   baseApiUrl: 'https://ng-nest-blog.herokuapp.com/api',
+  // baseApiUrl: 'http://localhost:3000/api',
 };
